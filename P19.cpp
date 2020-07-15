03e404fcbb9f012e3a8b0b653756bb9b0e34525f
Lê Ngọc Hoàng <60839552+Justinianus2001@users.noreply.github.com>
2020-07-15 19:23:34 +0700

    Update P19.cpp

--- P19.cpp
+++ P19.cpp
@@ -10,13 +10,13 @@ int main(){
 		dayBegin, dayEnd, monthBegin, monthEnd, yearBegin, yearEnd, weekDay = 2, cnt = 0, flag = 0;
 	cin >> dayBegin >> monthBegin >> yearBegin >> dayEnd >> monthEnd >> yearEnd;
 	for(int day = 1, month = 1, year = 1900; ; day ++, weekDay ++){
-		if(day > valid[month] + (month == 2 && isLeapYear(year)))			day = 1, month ++;
-		if(month > 12)														month = 1, year ++;
-		if(weekDay > 8)														weekDay -= 7;
+		if(day > valid[month] + (month == 2 && isLeapYear(year)))		day = 1, month ++;
+		if(month > 12)								month = 1, year ++;
+		if(weekDay > 8)								weekDay -= 7;
 		if(day == dayBegin && month == monthBegin && year == yearBegin)		flag = 1;
-		if(flag != 1)														continue;
-		if(day == 1 && weekDay == 8)										cnt ++;
-		if(day == dayEnd && month == monthEnd && year == yearEnd)			break;
+		if(flag != 1)								continue;
+		if(day == 1 && weekDay == 8)						cnt ++;
+		if(day == dayEnd && month == monthEnd && year == yearEnd)		break;
 	}
 	cout << cnt;
 	return 0;
@@ -25,6 +25,6 @@ int main(){
 //	URL:	https://projecteuler.net/problem=19
 //	Time:	O(n)
 //	Input:	1 1 1901
-//			31 12 2000
+//		31 12 2000
 //	Output:	171
 //	Lang:	C++
