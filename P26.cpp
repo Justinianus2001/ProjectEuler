49440243eea3088c4e8714acf287cb0d2e2e393c
Lê Ngọc Hoàng <60839552+Justinianus2001@users.noreply.github.com>
2020-07-17 12:32:19 +0700

    Add files via upload

---
+++ P26.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main(){
+	int inp, ans, maxCycle = 0, remain, cnt;
+	cin >> inp;
+	for(int idx = 2; idx < inp; idx ++){
+		map<int, int> mp;
+		remain = 1, cnt = 1;
+		while(true){
+			if(!(remain % idx))		goto NEXT;
+			remain %= idx;
+			if(mp[remain])			break;
+			mp[remain] = cnt;
+			while(remain < idx)		cnt ++, remain *= 10;
+		}
+		if(cnt - mp[remain] > maxCycle)		ans = idx, maxCycle = cnt - mp[remain];
+		NEXT:;
+	}
+	cout << ans;
+	return 0;
+}
+//	Title:	Problem 26 - Reciprocal cycles
+//	URL:	https://projecteuler.net/problem=26
+//	Time:	O(n^2)
+//	Input:	1000
+//	Output:	983
+//	Lang:	C++
