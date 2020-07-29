ca817ab20920cb4dfed5dbce225eb942d513833f
Lê Ngọc Hoàng <60839552+Justinianus2001@users.noreply.github.com>
2020-07-29 13:23:37 +0700

    Add files via upload

---
+++ P53.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+	vector<int> row;
+	int inp, lb, ans = 0;
+	cin >> inp >> lb;
+	for(int step = 0; step <= inp; step ++){
+		for(int idx = row.size() - 1; idx >= 0; idx --){
+			row[idx] += row[idx - 1];
+			if(row[idx] > lb)	row[idx] = lb + 1, ans ++;
+		}
+		row.push_back(1);
+	}
+	cout << ans;
+	return 0;
+}
+//	Title:	Problem 53 - Combinatoric selections
+//	URL:	https://projecteuler.net/problem=53
+//	Time:	O(?)
+//	Input:	100 1000000
+//	Output:	4075
+//	Lang:	C++
